1ce371b1aeb8ef4ada0a273389a847aacb88712c
Alex Diaz <alex@akdev.xyz>
2026-01-31 11:03:30 -0500

    chore: use commonjs

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,12 +1,10 @@
 import { createDefaultPreset } from "ts-jest";
 
 const tsJestTransformCfg = createDefaultPreset({
-  useESM: true,
 }).transform;
 
 export default {
   testEnvironment: "node",
-  extensionsToTreatAsEsm: [".ts"],
   roots: ['<rootDir>/src'],
   moduleNameMapper: {
     '^@akdev1l/constructs$': '<rootDir>/src/index.ts'
